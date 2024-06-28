6cdf51f91aa3da6a336cdac5939fd1bb2a9a762f
sunil <dipak.wwe1@gmail.com>
2024-06-28 09:48:44 +0530

    checkpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import {Signup} from "./pages/Signup"
 import { Signin } from "./pages/Signin"
+import {Dashboard} from "./pages/Dashboard"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
        <Routes>
         <Route path = "/signup" element = {<Signup/>}/>
         <Route path = "/signin" element = {<Signin/>}/>
+        <Route path = "/Dashboard" element = {<Dashboard/>}/>
 
        </Routes>
        </BrowserRouter>
